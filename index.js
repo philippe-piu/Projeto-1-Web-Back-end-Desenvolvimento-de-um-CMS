f998a3aca08e3a10efd74aab8641b0a981a49ea9
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2024-04-22 15:10:40 -0300

    Correção
    Criado uma pasta public e arrastado o css dentro dela isso faz funcionar o css.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express')
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Template Mustache
 var mustacheExpress = require("mustache-express");
