db51248e32fa9de76eb2909e8da651b4fd540d09
JPMontilha <jpvacilotomontilha@gmail.com>
2024-08-01 13:25:14 -0300

    feat: adicionando a possibilidade de editar a imagem da notícia.

--- Controller/edit.js
+++ Controller/edit.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const { loadNoticia } = require('../utils/noticiaLoader');
 const router = express.Router();
+const multer = require('multer'); // Importe o multer
+const upload = require('../utils/multerConfig'); // Importe a configuração do multer
 
 // Função para salvar uma notícia pelo ID
 const saveNoticia = (id, noticia) => {
@@ -21,20 +23,29 @@ router.get('/editar/:id', (req, res) => {
     } else {
       res.status(404).send('Notícia não encontrada');
     }
-  }else{
+  } else {
     res.redirect('/login');
   }
 });
 
 // Rota para processar a edição da notícia
-router.post('/editar/:id', (req, res) => {
+router.post('/editar/:id', upload.single('image'), (req, res) => {
   const id = req.params.id;
   const updatedData = req.body;
   const noticia = loadNoticia(id);
 
   if (noticia) {
+    // Atualize os dados da notícia com os dados enviados
     const updatedNoticia = { ...noticia, ...updatedData };
+    
+    // Se houver uma nova imagem enviada, atualize o nome do arquivo na notícia
+    if (req.file) {
+      updatedNoticia.image = req.file.filename;
+    }
+
+    // Salve a notícia atualizada
     saveNoticia(id, updatedNoticia);
+
     res.redirect('/home'); // Redirecione para a página principal após a edição
   } else {
     res.status(404).send('Notícia não encontrada');
